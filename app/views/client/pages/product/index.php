e8e79455da3a5834befcfa045cab2a7e4ee6781b
tuanemtramtinh <anh.nguyen2252038@hcmut.edu.vn>
2025-02-21 21:28:23 +0700

    update

--- app/views/client/pages/product/index.php
+++ app/views/client/pages/product/index.php
@@ -1,1 +1,21 @@
-<h2>TRANG DANH SÁCH SẢN PHẨM</h2>
+<div class="product">
+  <div class="container">
+    <div class="product__wrapper">
+      <h2 class="product__title">Products</h2>
+      <div class="product__list">
+        <div class="product__item">
+          <div class="product__item-image">
+            <img src="" alt="">
+          </div>
+          <h3 class="product__item-title">
+
+          </h3>
+          <div class="product__item-content">
+            <span class="product__item-price"></span>
+            <span class="product__item-volume"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
