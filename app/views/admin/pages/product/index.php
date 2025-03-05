d4a2938eff90f0138ce8635d7b67c16907867170
tuanemtramtinh <anhnguyentuan073@gmail.com>
2025-03-05 16:03:49 +0700

    check

--- app/views/admin/pages/product/index.php
+++ app/views/admin/pages/product/index.php
@@ -1,1 +1,60 @@
-<h2>hello</h2>
+<!-- Create Product -->
+<div class="section">
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title">Create Product</h4>
+        </div>
+        <div class="card-content">
+          <div class="card-body">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Product List -->
+<section class="section">
+  <div class="row">
+    <div class="col-12">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title">Product List</h4>
+        </div>
+        <div class="card-content">
+          <div class="card-body">
+            <table class="table table-hover table-responsive table-sm">
+              <thead>
+                <th>No.</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Action</th>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>
+                    <div>
+                      <img width="  0px" height="auto" src="images/product.png" alt="">
+                    </div>
+                  </td>
+                  <td>Best Perfume Ever</td>
+                  <td>$599</td>
+                  <td>
+                    <a class="btn btn-info btn-sm" href="#">Detail</a>
+                    <a class="btn btn-warning btn-sm ml-1" href="#">Edit</a>
+                    <a class="btn btn-danger btn-sm ml-1" href="#">Delete</a>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
