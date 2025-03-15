0893c0c391bf098c4dc6207454ec5af0c6e40442
tuanemtramtinh <anh.nguyen2252038@hcmut.edu.vn>
2025-03-15 17:35:12 +0700

    them trang chi tiet

--- app/controllers/admin/AdminBlog.php
+++ app/controllers/admin/AdminBlog.php
@@ -1,9 +1,19 @@
 <?php
-class AdminBlog extends Controller {
-  public function index(){
+class AdminBlog extends Controller
+{
+  public function index()
+  {
     $this->viewAdmin("layout", [
       "title" => "Blog",
       "page" => "blog/index"
+    ]);
+  }
+
+  public function detail()
+  {
+    $this->viewAdmin("layout", [
+      "title" => "Chi Tiết Blog",
+      "page" => "blog/detail"
     ]);
   }
 }
