dd8e9837160c1a652588a76cbdf413eed9f591ee
EricDuong5423 <tuduong05042003@gmail.com>
2025-03-19 07:51:23 +0700

    Done user overview

--- app/views/client/pages/user/profile.php
+++ app/views/client/pages/user/profile.php
@@ -1,1 +1,45 @@
-<h2>HỒ SƠ CÁ NHÂN</h2>
+<div class="user-profile__section1">
+    <div class="container">
+        <div class="section1__wrapper">
+            <div class="section1__avatar">
+                <img src="public/images/tt-avatar-1.png" alt="user avatar" style="width: 225px; height:225px;border-radius: 50%;border:5px solid rgba(171, 87, 45, 1)">
+                <img src="public/images/tt-avatar-2.png" alt="user avatar decorator" style="position:absolute;left:150px;top:160px">
+            </div>
+            <div class="section1__info">
+                <h3 class="section1__info-name">Ngô Ngọc Triệu Mẫn</h3>
+                <p class="section1__info-status">Premium Member since 2023</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="user-profile__section2">
+    <div class="container">
+        <div class="section2__wrapper">
+            <div class="section2__left-section">
+                <a href="user/profile" class="left-section__option" style="background-color:rgba(171, 87, 45, 1);padding:15px;border-radius:10px;">
+                    <img src="public/images/tt-option-1.png" alt="user icon" class="option__icon">
+                    <p class="option__text">Personal Information</p>
+                </a>
+                <a href="" class="left-section__option" style="padding:15px;">
+                    <img src="public/images/tt-option-2.png" alt="user icon" class="option__icon">
+                    <p class="option__text">Order History</p>
+                </a>
+                <a href="" class="left-section__option" style="margin: 0; padding: 15px;">
+                    <img src="public/images/tt-option-3.png" alt="user icon" class="option__icon">
+                    <p class="option__text">Personal Information</p>
+                </a>
+            </div>
+            <div class="section2__right-section">
+                <div class="right-section__user-details">
+                    <h4 class="section__title">
+                        Personal Information
+                    </h4>
+                    <form action="" class="info-section__form">
+
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
