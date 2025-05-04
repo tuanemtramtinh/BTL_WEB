b454ab65cda65c92b18fce6773c21dc9afc74cdc
Gragy08 <giaqui1712@gmail.com>
2025-05-04 10:49:00 +0700

    Giao diện nút thay đổi Logo HomeAdminPage

--- app/views/admin/pages/home/index.php
+++ app/views/admin/pages/home/index.php
@@ -1,6 +1,39 @@
+<div class="card mb-4">
+  <div class="card-header bg-primary text-white">Thay đổi Logo Website</div>
+  <div class="row">
+      <div class="col-12 col-lg-4">
+          <div class="card">
+              <div class="card-body">
+                  <div class="d-flex justify-content-center align-items-center flex-column">
+                      <div class="avatar avatar-4xl">
+                          <img src="../../../../../public/images/logo.svg" alt="logo" style="width: 150px; height: 150px;">
+                      </div>
+                      <h3 class="mt-3">Logo Hiện tại</h3>
+                  </div>
+              </div>
+          </div>
+      </div>
+      <div class="col-12 col-lg-8">
+          <div class="card">
+              <div class="card-body">
+                  <form action="/" method="POST" enctype="multipart/form-data">
+                      <div class="form-group">
+                          <label for="images" class="form-label">Image:</label>
+                          <input type="file" class="image-preview-filepond" name="imageLogo" id="Logo" multiple>
+                      </div>
+                      <div class="form-group">
+                          <button type="submit" class="btn btn-primary">Save Changes</button>
+                      </div>
+                  </form>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+
 <!-- Form 2: Đổi thông tin giới thiệu 1 -->
 <div class="card mb-4">
-    <div class="card-header bg-primary text-white">Thông tin giới thiệu 1</div>
+    <div class="card-header bg-secondary text-white">Thông tin giới thiệu 1</div>
     <div class="card-body">
     <form action="<?= BASE_URL ?>/admin/home/section2" method="POST">
         <div class="mb-3" style="margin-top: 20px;">
@@ -11,14 +44,14 @@
         <label for="intro1_content" class="form-label">Nội dung</label>
         <textarea class="form-control" name="intro1_content" id="intro1_content" rows="4" required><?= $data['intro1_content'] ?? '' ?></textarea>
         </div>
-        <button type="submit" class="btn btn-primary">Lưu giới thiệu 1</button>
+        <button type="submit" class="btn btn-secondary">Lưu giới thiệu 1</button>
     </form>
     </div>
 </div>
 
 <!-- Form 3: Đổi thông tin giới thiệu 2 -->
 <div class="card mb-4">
-    <div class="card-header bg-secondary text-white">Thông tin giới thiệu 2</div>
+    <div class="card-header bg-primary text-white">Thông tin giới thiệu 2</div>
     <div class="card-body">
     <form action="<?= BASE_URL ?>/admin/home/section3" method="POST">
         <div class="mb-3" style="margin-top: 20px;">
@@ -29,14 +62,14 @@
         <label for="intro2_content" class="form-label">Nội dung</label>
         <textarea class="form-control" name="intro2_content" id="intro2_content" rows="4" required><?= $data['intro2_content'] ?? '' ?></textarea>
         </div>
-        <button type="submit" class="btn btn-secondary text-white">Lưu giới thiệu 2</button>
+        <button type="submit" class="btn btn-primary text-white">Lưu giới thiệu 2</button>
     </form>
     </div>
 </div>
 
 <!-- Form 4: Đổi thông tin giảm giá -->
 <div class="card mb-4">
-  <div class="card-header bg-info text-white">Thông tin giảm giá</div>
+  <div class="card-header bg-secondary text-white">Thông tin giảm giá</div>
   <div class="card-body">
     <form action="<?= BASE_URL ?>/admin/home/section4" method="POST">
       <div class="mb-3" style="margin-top: 20px;">
@@ -51,7 +84,7 @@
         <label for="sale_content" class="form-label">Nội dung</label>
         <textarea class="form-control" name="sale_content" id="sale_content" rows="4" required><?= $data['sale_content'] ?? '' ?></textarea>
       </div>
-      <button type="submit" class="btn btn-info">Lưu thông tin giảm giá</button>
+      <button type="submit" class="btn btn-secondary">Lưu thông tin giảm giá</button>
     </form>
   </div>
 </div>
